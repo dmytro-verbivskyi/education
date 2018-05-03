c64676e8e3b30791d01c367e8b026a279c5855b0
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2018-05-03 18:37:07 +0300

    AkkaSpring: enable scheduling

--- src/main/java/boot/akkaspring/schedule/Scheduler.java
+++ src/main/java/boot/akkaspring/schedule/Scheduler.java
@@ -14,12 +14,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+@EnableScheduling
 @Component
 public class Scheduler {
     private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler.class);
