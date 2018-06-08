5b6d92604e1199d6bae18d0b9d4fa699d2d21011
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2018-06-08 17:12:28 +0300

    HumanReadableDuration: fine

--- src/main/java/codewars/HumanReadableDuration.java
+++ src/main/java/codewars/HumanReadableDuration.java
@@ -1,61 +1,54 @@
 package codewars;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 
-import java.util.concurrent.TimeUnit;
-
-import static java.util.concurrent.TimeUnit.DAYS;
-import static java.util.concurrent.TimeUnit.HOURS;
-import static java.util.concurrent.TimeUnit.MINUTES;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import java.util.List;
 
 class HumanReadableDuration {
 
-    private static final ImmutableMap<TimeUnit, Duration> DURATIONS = ImmutableMap.<TimeUnit, Duration>builder()
-            .put(DAYS, new Duration("day", " "))
-            .put(HOURS, new Duration("hour", " "))
-            .put(MINUTES, new Duration("minute", " "))
-            .put(SECONDS, new Duration("second", " and "))
-            .build();
-
     static String formatDuration(int seconds) {
         StringBuilder result = new StringBuilder();
-        int amountOfDays = seconds / 86400;
-        int amountOfHours = (seconds - (amountOfDays * 86400)) / 3600;
-        int amountOfMinutes = (seconds - (amountOfDays * 86400) - (amountOfHours * 3600)) / 60;
-        int amountOfSeconds = seconds - (amountOfDays * 86400) - (amountOfHours * 3600) - (amountOfMinutes * 60);
+        int amountOfYears = seconds / 31536000;
+        int amountOfDays = (seconds -= (amountOfYears * 31536000)) / 86400;
+        int amountOfHours = (seconds -= (amountOfDays * 86400)) / 3600;
+        int amountOfMinutes = (seconds -= (amountOfHours * 3600)) / 60;
+        int amountOfSeconds = seconds - (amountOfMinutes * 60);
 
+        List<String> durations = Lists.newArrayList();
+        if (amountOfYears > 0) {
+            durations.add(duration(amountOfYears, "year"));
+        }
         if (amountOfDays > 0) {
-            apply(result, amountOfDays, DAYS);
+            durations.add(duration(amountOfDays, "day"));
         }
         if (amountOfHours > 0) {
-            apply(result, amountOfHours, HOURS);
+            durations.add(duration(amountOfHours, "hour"));
         }
         if (amountOfMinutes > 0) {
-            apply(result, amountOfMinutes, MINUTES);
+            durations.add(duration(amountOfMinutes, "minute"));
         }
         if (amountOfSeconds > 0) {
-            apply(result, amountOfSeconds, SECONDS);
+            durations.add(duration(amountOfSeconds, "second"));
         }
 
-        String str = result.toString();
-        return str.isEmpty() ? "now" : str;
+        if (durations.isEmpty()) {
+            return "now";
+        }
+        if (durations.size() == 1) {
+            return durations.get(0);
+        }
+        for (int i = 0; i < durations.size() - 2; i++) {
+            result.append(durations.get(i)).append(", ");
+        }
+        return result.append(durations.get(durations.size() - 2))
+                .append(" and ")
+                .append(durations.get(durations.size() - 1))
+                .toString();
     }
 
-    private static void apply(StringBuilder result, int amount, TimeUnit unit) {
+    private static String duration(int amount, String unit) {
         boolean isPlural = (amount != 1);
-        result.append(result.length() > 0 ? DURATIONS.get(unit).appender : "")
-                .append(amount).append(" ")
-                .append(DURATIONS.get(unit).name).append((isPlural ? "s" : ""));
+        return String.format("%d %s%s", amount, unit, (isPlural ? "s" : ""));
     }
 
-    private static class Duration {
-        String name;
-        String appender;
-
-        Duration (String name, String appender) {
-            this.name = name;
-            this.appender = appender;
-        }
-    }
 }
--- src/test/java/codewars/HumanReadableDurationTest.java
+++ src/test/java/codewars/HumanReadableDurationTest.java
@@ -41,13 +41,34 @@ public class HumanReadableDurationTest {
 
     @Test
     public void hoursMinutesAndSecondsTest() throws Exception {
-        assertThat(formatDuration(3661)).isEqualTo("1 hour 1 minute and 1 second");
-        assertThat(formatDuration(64651)).isEqualTo("17 hours 57 minutes and 31 seconds");
+        assertThat(formatDuration(3661)).isEqualTo("1 hour, 1 minute and 1 second");
+        assertThat(formatDuration(64651)).isEqualTo("17 hours, 57 minutes and 31 seconds");
+        assertThat(formatDuration(64620)).isEqualTo("17 hours and 57 minutes");
+        assertThat(formatDuration(61201)).isEqualTo("17 hours and 1 second");
     }
 
     @Test
     public void daysTest() throws Exception {
         assertThat(formatDuration(86400)).isEqualTo("1 day");
         assertThat(formatDuration(259200)).isEqualTo("3 days");
+    }
+
+    @Test
+    public void daysHoursMinutesAndSecondsTest() throws Exception {
+        assertThat(formatDuration(86402)).isEqualTo("1 day and 2 seconds");
+        assertThat(formatDuration(263701)).isEqualTo("3 days, 1 hour, 15 minutes and 1 second");
+        assertThat(formatDuration(260100)).isEqualTo("3 days and 15 minutes");
+    }
+
+    @Test
+    public void yearsTest() throws Exception {
+        assertThat(formatDuration(31536000)).isEqualTo("1 year");
+        assertThat(formatDuration(5 * 31536000)).isEqualTo("5 years");
+    }
+
+    @Test
+    public void yearsAndAllOtherTest() throws Exception {
+        assertThat(formatDuration(31536062)).isEqualTo("1 year, 1 minute and 2 seconds");
+        assertThat(formatDuration(5 * 31536000 + 364 * 86400)).isEqualTo("5 years and 364 days");
     }
 }
