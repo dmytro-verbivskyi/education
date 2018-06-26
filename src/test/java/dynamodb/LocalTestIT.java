0db34becdccd1b980ea4051eef64449b816e7fce
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2018-06-26 14:55:46 +0300

    Amazon DynamoDb localhost unit testing

--- src/test/java/dynamodb/LocalTestIT.java
+++ src/test/java/dynamodb/LocalTestIT.java
@@ -1,12 +1,22 @@
 package dynamodb;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import dynamodb.util.LocalDynamoDBCreationRule;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,62 +26,41 @@ public class LocalTestIT {
     @ClassRule
     public static final LocalDynamoDBCreationRule dynamoDB = new LocalDynamoDBCreationRule();
 
-    private AmazonDynamoDB dynamoClient = dynamoDB.getAmazonDynamoDB();
+    private static AmazonDynamoDB dynamoClient;
+
+    private static final String MY_TABLE_NAME = "Movies";
 
     // README
     // first run: ./gradlew clean test
     // it will copy libs with custom gradle task properly. Then you can develop your tests.
 
+    @BeforeClass
+    public static void setupTable() {
+        dynamoClient = dynamoDB.getAmazonDynamoDB();
+
+        System.out.println("Attempting to create table; please wait...");
+        CreateTableResult createTableResult = dynamoClient.createTable(
+                Arrays.asList(
+                        new AttributeDefinition("year", ScalarAttributeType.N),
+                        new AttributeDefinition("title", ScalarAttributeType.S)),
+                MY_TABLE_NAME,
+                Arrays.asList(
+                        new KeySchemaElement("year", KeyType.HASH),     // Partition key
+                        new KeySchemaElement("title", KeyType.RANGE))   // Sort key
+                , new ProvisionedThroughput(10L, 10L));
+
+        TableDescription tableDescription = createTableResult.getTableDescription();
+        assertThat(tableDescription.getTableStatus()).isEqualTo("ACTIVE");
+        assertThat(tableDescription.getTableName()).isEqualTo(MY_TABLE_NAME);
+        assertThat(tableDescription.getItemCount()).isZero();
+    }
+
     @Test
-    public void emptyDatabase() throws Exception {
+    public void listTables() throws Exception {
         ListTablesResult listTablesResult = dynamoClient.listTables();
 
         assertThat(listTablesResult).isNotNull();
-        assertThat(listTablesResult.getTableNames()).isEmpty();
+        assertThat(listTablesResult.getTableNames()).containsOnly(MY_TABLE_NAME);
     }
-//
-//    @Test
-//    public void selectFromTable() throws Exception {
-//        ListTablesResult listTablesResult = dynamoClient.listTables();
-//
-//        assertThat(listTablesResult).isNotNull();
-//        assertThat(listTablesResult.getTableNames()).isEmpty();
-//
-//        createSomeTables();
-//
-//        listTablesResult = dynamoClient.listTables();
-//
-//        assertThat(listTablesResult).isNotNull();
-//        assertThat(listTablesResult.getTableNames()).isEmpty();
-//    }
 
-//    private void createSomeTables() {
-//        DynamoDBMapper mapper = new DynamoDBMapper(dynamoClient);
-//
-//        CreateTableRequest tableRequest = mapper.generateCreateTableRequest(EntityOne.class);
-//        tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
-//        dynamoClient.createTable(tableRequest);
-//    }
-//
-//    @DynamoDBTable(tableName = "entityOne")
-//    private class EntityOne {
-//        public String id;
-//        public String title;
-//
-//        public String getId() {
-//            return id;
-//        }
-//
-//        public void setId(String id) {
-//            this.id = id;
-//        }
-//
-//        public String getTitle() {
-//            return title;
-//        }
-//
-//        public void setTitle(String title) {
-//            this.title = title;
-//        }
-//    }
 }
