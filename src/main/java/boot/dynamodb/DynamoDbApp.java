d861c1894e8508ea03e2f56bce13bc6150b4d217
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2018-07-09 17:25:41 +0300

    DynamoDb Context Test

--- src/main/java/boot/dynamodb/DynamoDbApp.java
+++ src/main/java/boot/dynamodb/DynamoDbApp.java
@@ -1,9 +1,12 @@
 package boot.dynamodb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DynamoDbApp {
 
     public static void main(String[] args) {
