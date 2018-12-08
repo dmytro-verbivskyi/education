c4683644e011a9eb49b86450fdd71166ccb1e91e
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2018-12-08 21:34:31 +0200

    assertJ feature overview

--- src/test/java/assertj/FileAssertTest.java
+++ src/test/java/assertj/FileAssertTest.java
@@ -7,6 +7,8 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,12 +26,26 @@ public class FileAssertTest {
 
     @Test
     public void withFile() throws IOException {
-        File file = temporaryFolder.newFile("file.with.music.mp3");
+        File file = temporaryFolder.newFile("file.with.mp3");
 
-        assertThat(file).hasName("file.with.music.mp3").hasExtension("mp3")
+        assertThat(file).hasName("file.with.mp3").hasExtension("mp3")
                 .canRead().canWrite()
                 .hasContent("")
                 .hasSameContentAs(temporaryFolder.newFile(), Charset.defaultCharset());
+    }
+
+    @Test
+    public void withPath() throws IOException {
+        File file = temporaryFolder.newFile("hello.mp3");
+
+        Path path = Paths.get(file.toURI());
+        Path tempFolderPath = Paths.get(temporaryFolder.getRoot().toURI());
+        assertThat(path).isAbsolute()
+                .isCanonical()
+                .isRegularFile()
+                .isNormalized()
+                .hasParent(tempFolderPath)
+                .exists();
     }
 
 }
--- src/test/java/assertj/PathAndUriAssertTest.java
+++
@@ -1,4 +0,0 @@
-package assertj;
-
-public class PathAndUriAssertTest {
-}
