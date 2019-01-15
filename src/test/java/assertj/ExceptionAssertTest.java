bced22ba2ae728e682c49dc487685b331dc84457
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2019-01-15 22:43:27 +0200

    assertJ feature overview

--- src/test/java/assertj/ExceptionAssertTest.java
+++ src/test/java/assertj/ExceptionAssertTest.java
@@ -1,4 +1,41 @@
 package assertj;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ExceptionAssertTest {
+
+    @Test
+    public void simpleExceptionValidation() {
+        String message = "OMG!";
+        assertThatThrownBy(() -> methodWithThrowOfRuntimeException(message))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("OMG!")
+                .hasMessageEndingWith("G!")
+                .hasNoCause();
+    }
+
+    @Test
+    public void rethrownExceptionValidation() {
+        assertThatThrownBy(() -> {
+            try {
+                throw new IOException("You must catch me");
+            } catch (IOException e) {
+                throw new IllegalArgumentException("OMG 42 != 43", e);
+            }
+        })
+                .isNotExactlyInstanceOf(NumberFormatException.class)
+                .hasMessage("OMG %d != %d", 42, 43)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasCauseInstanceOf(IOException.class)
+                .hasStackTraceContaining("You must catch me")
+        ;
+    }
+
+    private void methodWithThrowOfRuntimeException(String message) {
+        throw new RuntimeException(message);
+    }
 }
--- src/test/java/assertj/FutureAssertTest.java
+++ src/test/java/assertj/FutureAssertTest.java
@@ -1,4 +1,24 @@
 package assertj;
 
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FutureAssertTest {
+
+    @Test
+    public void name() throws ExecutionException, InterruptedException {
+        Future<String> future = Executors.newCachedThreadPool().submit(() -> {
+            if (true) {
+                throw new RuntimeException("Hello");
+            }
+            return "World";
+        });
+
+        assertThat(future).isDone();
+    }
 }
