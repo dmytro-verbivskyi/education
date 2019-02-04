c07d62c76d7859149365aee44386d7b4a23398c4
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2019-02-04 15:01:18 +0200

    overriding test

--- src/test/java/interview/OverridingTest.java
+++ src/test/java/interview/OverridingTest.java
@@ -9,50 +9,50 @@ public class OverridingTest {
     @Test
     public void overridingVsHiding() throws Exception {
         A a = new A();
-        assertThat(a.aMethod()).isEqualTo("A");
-        assertThat(a.bField).isEqualTo("B");
-        assertThat(a.cMethod()).isEqualTo("C");
-        assertThat(a.dField).isEqualTo("D");
+        assertThat(a.staticMethodA()).isEqualTo("A");
+        assertThat(a.staticFieldB).isEqualTo("B");
+        assertThat(a.instanceMethodC()).isEqualTo("C");
+        assertThat(a.instanceFieldD).isEqualTo("D");
 
         a = new B();
-        assertThat(a.aMethod()).isEqualTo("A");
-        assertThat(a.bField).isEqualTo("B");
-        assertThat(a.cMethod()).as("real overriding").isEqualTo("C1");
-        assertThat(a.dField).as("taking by ref").isEqualTo("D");
+        assertThat(a.staticMethodA()).isEqualTo("A");
+        assertThat(a.staticFieldB).isEqualTo("B");
+        assertThat(a.instanceMethodC()).as("real overriding").isEqualTo("C1");
+        assertThat(a.instanceFieldD).as("taking by ref").isEqualTo("D");
 
         B b = new B();
-        assertThat(b.aMethod()).isEqualTo("A1");
-        assertThat(b.bField).isEqualTo("B1");
-        assertThat(b.cMethod()).isEqualTo("C1");
-        assertThat(b.dField).isEqualTo("D1");
+        assertThat(b.staticMethodA()).isEqualTo("A1");
+        assertThat(b.staticFieldB).isEqualTo("B1");
+        assertThat(b.instanceMethodC()).isEqualTo("C1");
+        assertThat(b.instanceFieldD).isEqualTo("D1");
     }
 
     private static class A {
-        public static String aMethod() {
+        static String staticMethodA() {
             return "A";
         }
 
-        public static String bField = "B";
+        static String staticFieldB = "B";
 
-        public String cMethod() {
+        String instanceMethodC() {
             return "C";
         }
 
-        public String dField = "D";
+        String instanceFieldD = "D";
     }
 
     private static class B extends A {
-        public static String aMethod() {
+        static String staticMethodA() {
             return "A1";
         }
 
-        public static String bField = "B1";
+        static String staticFieldB = "B1";
 
-        public String cMethod() {
+        String instanceMethodC() {
             return "C1";
         }
 
-        public String dField = "D1";
+        String instanceFieldD = "D1";
     }
 
 }
