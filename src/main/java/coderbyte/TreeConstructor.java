5887f1e17ade9fbbf6426dd31cc0d442ac07f89f
Dmytro Verbivskyi <dmytro.verbivskyi@gettyimages.com>
2021-01-05 00:29:22 +0200

    TreeConstructor

--- src/main/java/coderbyte/TreeConstructor.java
+++ src/main/java/coderbyte/TreeConstructor.java
@@ -1,15 +1,89 @@
 package coderbyte;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class TreeConstructor {
 
     public static String TreeConstructor(String... strArr) {
-        // code goes here
-        return strArr[0];
+        Pattern DIGITS = Pattern.compile("(\\d+)");
+        for (String onePair : strArr) {
+            Matcher matcher = DIGITS.matcher(onePair);
+            matcher.find();
+            Integer child = Integer.valueOf(matcher.group());
+            matcher.find();
+            Integer parent = Integer.valueOf(matcher.group());
+
+            if (!registerPair(parent, child)) {
+                return "false";
+            }
+        }
+
+        return "true";
     }
 
-    public static void main (String[] args) {
+    static Map<Integer, Node> TREE_AS_LIST = new HashMap<>();
+
+    private static boolean registerPair(Integer parent, Integer child) {
+        Node parentNode = TREE_AS_LIST.get(parent);
+        Node childNode = TREE_AS_LIST.get(child);
+
+        if (isNull(parentNode)) {
+            parentNode = new Node().value(parent);
+            TREE_AS_LIST.put(parent, parentNode);
+        }
+        if (isNull(childNode)) {
+            childNode = new Node().value(child);
+            TREE_AS_LIST.put(child, childNode);
+        }
+
+        if (nonNull(childNode.parent)) {
+            return false; // child already has parent
+        }
+        if (nonNull(parentNode.childOne) && nonNull(parentNode.childTwo)) {
+            return false; // parent already has two children
+        }
+        childNode.parent(parentNode);
+        if (isNull(parentNode.childOne)) {
+            parentNode.childOne(childNode);
+        } else {
+            parentNode.childTwo(childNode);
+        }
+        return true;
+    }
+
+    static class Node {
+        Integer value;
+        Node parent;
+        Node childOne;
+        Node childTwo;
+
+        public Node value(Integer value) {
+            this.value = value;
+            return this;
+        }
+
+        public Node parent(Node parent) {
+            this.parent = parent;
+            return this;
+        }
+
+        public Node childOne(Node childOne) {
+            this.childOne = childOne;
+            return this;
+        }
+
+        public Node childTwo(Node childTwo) {
+            this.childTwo = childTwo;
+            return this;
+        }
+    }
+
+    public static void main(String[] args) {
         // keep this function call here
         Scanner s = new Scanner(System.in);
         System.out.print(TreeConstructor(s.nextLine()));
--- src/test/java/coderbyte/TreeConstructorTest.java
+++ src/test/java/coderbyte/TreeConstructorTest.java
@@ -9,16 +9,38 @@ public class TreeConstructorTest {
 
     @Test
     public void first() {
-        assertThat(TreeConstructor("(1111,2)", "(2,44)", "(7,2)")).isEqualTo("true");
+        assertThat(TreeConstructor("(1,2)", "(2,4)", "(7,2)")).isEqualTo("true");
     }
 
     @Test
     public void second() {
         assertThat(TreeConstructor("(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)")).isEqualTo("true");
+        /*   4 - 2 - 7 - 5 - 9
+                  \
+                   1         */
     }
 
     @Test
     public void third() {
         assertThat(TreeConstructor("(1,2)", "(3,2)", "(2,12)", "(5,2)")).isEqualTo("false");
+        /*  12 - 2 - 3
+                / \
+               5   1         */
+    }
+
+    @Test
+    public void forth() {
+        assertThat(TreeConstructor("(1,2)", "(3,2)", "(2,12)", "(5,7), (4,5)")).isEqualTo("false");
+        /*  12 - 2 - 3      7 - 5 - 4
+                  \
+                   1         */
+    }
+
+    @Test
+    public void fifth() {
+        assertThat(TreeConstructor("(1,2)", "(3,2)", "(2,12)", "(5,3)", "(6,5)", "(4,1)", "(6,4)")).isEqualTo("false");
+        /*  12 - 2 - 3 - 5
+                  \       \
+                   1 - 4 - 6        */
     }
 }
